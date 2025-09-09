7d074737e0c3519055cfca34f5bd2c4557817af2
zk8619 <zk8619@icloud.com>
2025-09-08 19:19:11 -0500

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  classroomEndpoint: "https://script.google.com/a/macros/newcovenant.net/s/AKfycbypk7MnPAxicy4LuVriNxxf3jubiRpSBzGxrlVyQhVaOTKIGO7HNZLxmMFLVEVjSzKprQ/exec",
+  classroomEndpoint: "https://script.google.com/macros/s/AKfycbypk7MnPAxicy4LuVriNxxf3jubiRpSBzGxrlVyQhVaOTKIGO7HNZLxmMFLVEVjSzKprQ/exec",
   autoSyncOnLoad: true,
-  useJsonp: true   // <— turn this on
+  useJsonp: true   // keep JSONP on for mobile (avoids CORS)
 };
